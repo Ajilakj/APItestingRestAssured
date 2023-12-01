b649103d21a1e2cf0a1787907430efa854aa75f4
Ajila K Johnkutty <kjajila@gmail.com>
2023-12-01 16:12:25 -0500

    add GET, PUT,DELETE

--- src/test/java/restAssured/Basics.java
+++ src/test/java/restAssured/Basics.java
@@ -7,6 +7,7 @@ public class Basics {
 
 	public static void main(String[] args) {
 		RestAssured.baseURI="https://rahulshettyacademy.com";
+		// POST 
 		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
 		.body("{\r\n"
 				+ "    \"location\": {\r\n"
@@ -26,5 +27,35 @@ public class Basics {
 				+ "}")
 		.when().post("/maps/api/place/add/json")
 		.then().log().all().assertThat().statusCode(200);
+		
+		// PUT 
+		given().log().all().queryParam("key", "qaclick123").queryParam("place_id", "b60776a46df67a8b1e64812c2ef5aa18")
+		.header("Content-Type", "application/json").body("{\r\n"
+				+ "\"place_id\":\"b60776a46df67a8b1e64812c2ef5aa18\",\r\n"
+				+ "\"address\":\"70 Summer walk, USA\",\r\n"
+				+ "\"key\":\"qaclick123\"\r\n"
+				+ "}")
+		.when().put("/maps/api/place/update/json")
+		.then().log().all().assertThat().statusCode(200);
+		
+
+		// GET 
+		given().log().all().queryParam("key", "qaclick123").queryParam("place_id", "3afe61be24407e805c2882c5ae17bb95")
+		.header("Content-Type", "application/json")
+		.when().get("/maps/api/place/get/json")
+		.then().log().all().assertThat().statusCode(200);
+		
+		
+		// DELETE 
+		given().log().all().queryParam("key", "qaclick123")
+		.header("Content-Type", "application/json").body("{\r\n"
+				+ "    \"place_id\":\"3afe61be24407e805c2882c5ae17bb95\"\r\n"
+				+ "}")
+		.when().delete("/maps/api/place/delete/json")
+		.then().log().all().assertThat().statusCode(200);
+		
+		
+
+			
 	}
 }
