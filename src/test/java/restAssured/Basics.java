9b7f49f1a7139a6085c49e345c430f073caa8085
Ajila K Johnkutty <kjajila@gmail.com>
2023-12-06 15:24:54 -0500

    use payload

--- src/test/java/restAssured/Basics.java
+++ src/test/java/restAssured/Basics.java
@@ -5,28 +5,15 @@ import io.restassured.path.json.JsonPath;
 
 import static io.restassured.RestAssured.*;
 
+import files.Payload;
+
 public class Basics {
 
 	public static void main(String[] args) {
 		RestAssured.baseURI="https://rahulshettyacademy.com";
 		// POST 
 		String response=given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-		.body("{\r\n"
-				+ "    \"location\": {\r\n"
-				+ "        \"lat\": -38.383494,\r\n"
-				+ "        \"lng\": 33.427362\r\n"
-				+ "    },\r\n"
-				+ "    \"accuracy\": 50,\r\n"
-				+ "    \"name\": \"Frontline house\",\r\n"
-				+ "    \"phone_number\": \"(+91) 983 893 3937\",\r\n"
-				+ "    \"address\": \"29, side layout, cohen 09\",\r\n"
-				+ "    \"types\": [\r\n"
-				+ "        \"shoe park\",\r\n"
-				+ "        \"shop\"\r\n"
-				+ "    ],\r\n"
-				+ "    \"website\": \"http://google.com\",\r\n"
-				+ "    \"language\": \"French-IN\"\r\n"
-				+ "}")
+		.body(Payload.addPlace())
 		.when().post("/maps/api/place/add/json")
 		.then().assertThat().statusCode(200).extract().response().asString();
 		
