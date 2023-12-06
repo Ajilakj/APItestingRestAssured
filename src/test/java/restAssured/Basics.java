5f6d92d138d2d404f25e0a262ac405993fedbb0f
Ajila K Johnkutty <kjajila@gmail.com>
2023-12-06 15:09:51 -0500

    use JSON parsing to extract data

--- src/test/java/restAssured/Basics.java
+++ src/test/java/restAssured/Basics.java
@@ -1,6 +1,8 @@
 package restAssured;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
 import static io.restassured.RestAssured.*;
 
 public class Basics {
@@ -8,7 +10,7 @@ public class Basics {
 	public static void main(String[] args) {
 		RestAssured.baseURI="https://rahulshettyacademy.com";
 		// POST 
-		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+		String response=given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
 		.body("{\r\n"
 				+ "    \"location\": {\r\n"
 				+ "        \"lat\": -38.383494,\r\n"
@@ -26,33 +28,39 @@ public class Basics {
 				+ "    \"language\": \"French-IN\"\r\n"
 				+ "}")
 		.when().post("/maps/api/place/add/json")
-		.then().log().all().assertThat().statusCode(200);
+		.then().assertThat().statusCode(200).extract().response().asString();
 		
-		// PUT 
-		given().log().all().queryParam("key", "qaclick123").queryParam("place_id", "b60776a46df67a8b1e64812c2ef5aa18")
-		.header("Content-Type", "application/json").body("{\r\n"
-				+ "\"place_id\":\"b60776a46df67a8b1e64812c2ef5aa18\",\r\n"
-				+ "\"address\":\"70 Summer walk, USA\",\r\n"
-				+ "\"key\":\"qaclick123\"\r\n"
-				+ "}")
-		.when().put("/maps/api/place/update/json")
-		.then().log().all().assertThat().statusCode(200);
+		System.out.println(response);
+		JsonPath js=new JsonPath(response); //for parsing Json
+		String placeId=js.getString("place_id");
 		
-
-		// GET 
-		given().log().all().queryParam("key", "qaclick123").queryParam("place_id", "3afe61be24407e805c2882c5ae17bb95")
-		.header("Content-Type", "application/json")
-		.when().get("/maps/api/place/get/json")
-		.then().log().all().assertThat().statusCode(200);
+		System.out.println(placeId);
 		
-		
-		// DELETE 
-		given().log().all().queryParam("key", "qaclick123")
-		.header("Content-Type", "application/json").body("{\r\n"
-				+ "    \"place_id\":\"3afe61be24407e805c2882c5ae17bb95\"\r\n"
-				+ "}")
-		.when().delete("/maps/api/place/delete/json")
-		.then().log().all().assertThat().statusCode(200);
+//		// PUT 
+//		given().log().all().queryParam("key", "qaclick123").queryParam("place_id", "b60776a46df67a8b1e64812c2ef5aa18")
+//		.header("Content-Type", "application/json").body("{\r\n"
+//				+ "\"place_id\":\"b60776a46df67a8b1e64812c2ef5aa18\",\r\n"
+//				+ "\"address\":\"70 Summer walk, USA\",\r\n"
+//				+ "\"key\":\"qaclick123\"\r\n"
+//				+ "}")
+//		.when().put("/maps/api/place/update/json")
+//		.then().log().all().assertThat().statusCode(200);
+//		
+//
+//		// GET 
+//		given().log().all().queryParam("key", "qaclick123").queryParam("place_id", "3afe61be24407e805c2882c5ae17bb95")
+//		.header("Content-Type", "application/json")
+//		.when().get("/maps/api/place/get/json")
+//		.then().log().all().assertThat().statusCode(200);
+//		
+//		
+//		// DELETE 
+//		given().log().all().queryParam("key", "qaclick123")
+//		.header("Content-Type", "application/json").body("{\r\n"
+//				+ "    \"place_id\":\"3afe61be24407e805c2882c5ae17bb95\"\r\n"
+//				+ "}")
+//		.when().delete("/maps/api/place/delete/json")
+//		.then().log().all().assertThat().statusCode(200);
 		
 		
 
