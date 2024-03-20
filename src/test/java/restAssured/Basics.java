203b7552e23b55cb8d1e6349d34c36bbd60cca76
Ajilakj <kjajila@gmail.com>
2024-03-19 22:34:14 -0400

    add delete request

--- src/test/java/restAssured/Basics.java
+++ src/test/java/restAssured/Basics.java
@@ -25,38 +25,38 @@ public class Basics {
 		String placeId=js.getString("place_id");
 		
 		System.out.println(placeId);
-//		
-//		// PUT 
-//		String newAddress = "Summer Walk, Africa";
-//		given().log().all().queryParam("key", "qaclick123")
-//		.header("Content-Type", "application/json").body("{\r\n"
-//				+"\"place_id\":\""+placeId+"\",\r\n" 
-//				+ "\"address\":\""+newAddress+"\",\r\n"
-//				+ "\"key\":\"qaclick123\"\r\n"
-//				+ "}")
-//		.when().put("/maps/api/place/update/json")
-//		.then().assertThat().statusCode(200).body("msg", equalTo("Address successfully updated"));
-//		
-//
-//		// GET 
-//		String getPlaceResponse = given().log().all().queryParam("key", "qaclick123")
-//				.queryParam("place_id",placeId)
-//				.when().get("maps/api/place/get/json")
-//				.then().assertThat().log().all().statusCode(200).extract().response().asString();
-//		
-//			JsonPath js1=new JsonPath(getPlaceResponse);
-//			String actualAddress =js1.getString("address");
-//			System.out.println(actualAddress);
-//			Assert.assertEquals(actualAddress, newAddress);
-//		
 		
-//		// DELETE 
-//		given().log().all().queryParam("key", "qaclick123")
-//		.header("Content-Type", "application/json").body("{\r\n"
-//				+ "    \"place_id\":\"3afe61be24407e805c2882c5ae17bb95\"\r\n"
-//				+ "}")
-//		.when().delete("/maps/api/place/delete/json")
-//		.then().log().all().assertThat().statusCode(200);
+		// PUT 
+		String newAddress = "Summer Walk, Africa";
+		given().log().all().queryParam("key", "qaclick123")
+		.header("Content-Type", "application/json").body("{\r\n"
+				+"\"place_id\":\""+placeId+"\",\r\n" 
+				+ "\"address\":\""+newAddress+"\",\r\n"
+				+ "\"key\":\"qaclick123\"\r\n"
+				+ "}")
+		.when().put("/maps/api/place/update/json")
+		.then().assertThat().statusCode(200).body("msg", equalTo("Address successfully updated"));
+		
+
+		// GET 
+		String getPlaceResponse = given().log().all().queryParam("key", "qaclick123")
+				.queryParam("place_id",placeId)
+				.when().get("maps/api/place/get/json")
+				.then().assertThat().log().all().statusCode(200).extract().response().asString();
+		
+			JsonPath js1=new JsonPath(getPlaceResponse);
+			String actualAddress =js1.getString("address");
+			System.out.println(actualAddress);
+			Assert.assertEquals(actualAddress, newAddress);
+		
+		
+		// DELETE 
+		given().log().all().queryParam("key", "qaclick123")
+		.header("Content-Type", "application/json").body("{\r\n"
+				+"\"place_id\":\""+placeId+"\",\r\n" 
+				+ "}")
+		.when().delete("/maps/api/place/delete/json")
+		.then().log().all().assertThat().statusCode(200);
 	}
 
 //	public static void string_manipulation() {
